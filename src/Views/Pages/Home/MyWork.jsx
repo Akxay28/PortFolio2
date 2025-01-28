e17a8654ce6c290d9df81fc0b4a737e3b05974d2
Dhaval <rnwdhaval1@gmail.com>
2025-01-28 15:39:11 +0530

    imageproject

--- src/Views/Pages/Home/MyWork.jsx
+++ src/Views/Pages/Home/MyWork.jsx
@@ -8,11 +8,17 @@ const MyWork = () => {
                 <h2 style={{ fontFamily: "Playwrite AU SA, serif", borderBottom: "2px solid #94E214", display: 'inline-block' }} className='mt-0 mt-lg-5 mb-0 mb-lg-5'>Latest Works</h2>
                 <span className='dashh2'></span>
                 <div className="row">
-                    <div className="col-lg-6">
+                    <div className="col-lg-3">
                         <img src={"yagnesh.png"} className='img-fluid' alt="" />
                     </div>
-                    <div className="col-lg-6">
-                        <img src={"Untitled-1.png"} className='img-fluid' alt="" />
+                    <div className="col-lg-3">
+                        <img src={"asion.png"} className='img-fluid' alt="" />
+                    </div>
+                    <div className="col-lg-3">
+                        <img src={"PIZZA.png"} className='img-fluid' alt="" />
+                    </div>
+                    <div className="col-lg-3">
+                        <img src={"cake.png"} className='img-fluid' alt="" />
                     </div>
                 </div>
             </div>
