dc9fa2d5e07c0f4f984f2284fb67c2d233e7cd4e
Dhaval <rnwdhaval1@gmail.com>
2025-01-28 14:23:34 +0530

    yagnesh

--- src/Views/Pages/Home/MyWork.jsx
+++ src/Views/Pages/Home/MyWork.jsx
@@ -8,14 +8,12 @@ const MyWork = () => {
                 <h2 style={{ fontFamily: "Playwrite AU SA, serif", borderBottom: "2px solid #94E214", display: 'inline-block' }} className='mt-0 mt-lg-5 mb-0 mb-lg-5'>Latest Works</h2>
                 <span className='dashh2'></span>
                 <div className="row">
-                    <div className="col-lg-4">
+                    <div className="col-lg-6">
+                        <img src={"yagnesh.png"} className='img-fluid' alt="" />
+                    </div>
+                    <div className="col-lg-6">
                         <img src={"Untitled-1.png"} className='img-fluid' alt="" />
                     </div>
-                    <div className="col-lg-4">
-                        <img src={"Untitled-1.png"} className='img-fluid' alt="" />
-                    </div>
-
-
                 </div>
             </div>
         </>
