c25eff6e033210188ffa797c6d656721efadd5af
Dhaval Patel <rnwdhaval1@gmail.com>
2025-02-03 16:21:38 +0530

    icons

--- src/Views/Layout/Footer.jsx
+++ src/Views/Layout/Footer.jsx
@@ -3,27 +3,27 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-            <div className="container mt-0 mt-lg-5" data-aos="fade-up">
+            <div className="container mt-0 mt-lg-5" >
 
-                <div className="row py-5">
+                <div className="row py-5" data-aos="fade-up">
 
                     <span className='display-4 text-center mb-5 py-5' >
                         <span className='p'></span>I am not hard to find let's create something truly <span style={{ fontFamily: "Playwrite AU SA, serif", borderBottom: "2px solid white", color: "#94E214" }}>spectacular !</span>   <span className='dashp'></span></span>
 
-                    <div className="col" data-aos="fade-up"
+                    <div className="col"
                         data-aos-anchor-placement="top-center">
-                        <span className='i' data-aos="fade-up"
+                        <span className='i'
                             data-aos-anchor-placement="top-center"></span>
                         <a href='https://github.com/Akxay28' target='_blank' class="fa-brands similarFont fs-1 fa-github"></a>
                         <span className='dashi'></span>
                     </div>
-                    <div className="col" data-aos="fade-up"
+                    <div className="col"
                         data-aos-anchor-placement="top-center">
                         <span className='i'></span>
                         <a href='https://www.linkedin.com/in/akshay-pal-a91528283' target='_blank' class="fa-brands similarFont fs-1 fa-linkedin"></a>
                         <span className='dashi'></span>
                     </div>
-                    <div className="col" data-aos="fade-up"
+                    <div className="col"
                         data-aos-anchor-placement="top-center">
                         <span className='i'></span>
                         <a href='https://www.instagram.com/qr/' target='_blank' class="fa-brands similarFont fs-1 fa-instagram"></a>
@@ -31,7 +31,7 @@ const Footer = () => {
                     </div>
 
                 </div>
-                <span className='dashbody' data-aos="fade-up-right"></span><br /><br />
+                <span className='dashbody'  ></span><br /><br />
                 <span className='dashhtml'></span>
             </div>
         </>
